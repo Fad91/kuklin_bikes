fedf47b098bd277c191effea1e12f0c5a16291d7
Антон Куклин <homyak91@inbox.ru>
2021-07-04 17:06:49 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,11 @@
 // Файл скрипта
+//  Variables
+
+const mainMenu = document.querySelector(".main-menu");
+const navButton = document.querySelector(".nav__button");
+
+// Navigation
+
+navButton.addEventListener("click", function () {
+  mainMenu.classList.toggle("main-menu--closed");
+});
