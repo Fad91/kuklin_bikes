f3bb557321e75b9357e38b1549d8ca103cf12abb
Антон Куклин <homyak91@inbox.ru>
2021-07-06 21:31:14 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,11 @@
 //  Variables
 
 const mainMenu = document.querySelector(".main-menu");
+const menuLink = document.querySelector("main-menu__item a");
 const navButton = document.querySelector(".nav__button");
+const phoneInput = document.querySelector("#tel");
+const form = document.querySelector(".feedback-form");
+let sections = Array.from(document.querySelectorAll("section"));
 
 // Navigation
 
@@ -20,3 +24,27 @@ navButton.addEventListener("click", function () {
 });
 
 // Blocking scroll
+
+// Validation
+
+form.addEventListener("invalid", function () {
+  return false;
+});
+
+phoneInput.addEventListener("invalid", function () {
+  let re = /^\d[\d\(\)\ -]{4,14}\d$/;
+  let valid = re.test(phoneInput.textContent);
+  if (valid) {
+    phoneInput.setCustomValidity("");
+  } else {
+    phoneInput.setCustomValidity("Поле должно состоять из цифр");
+  }
+});
+
+// Smooth scroll
+
+menuLink.addEventListener("click", function () {
+  sections.forEach(function (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  });
+});
