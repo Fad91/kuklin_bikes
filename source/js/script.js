2915ef850c2c255c69f64b6455ec338ec8fae9af
Антон Куклин <homyak91@inbox.ru>
2021-07-06 19:36:39 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -6,6 +6,17 @@ const navButton = document.querySelector(".nav__button");
 
 // Navigation
 
+mainMenu.classList.add("main-menu--closed");
+
 navButton.addEventListener("click", function () {
   mainMenu.classList.toggle("main-menu--closed");
+  mainMenu.classList.toggle("main-menu--opened");
+  if (mainMenu.classList.contains("main-menu--opened")) {
+    console.log(1);
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
 });
+
+// Blocking scroll
