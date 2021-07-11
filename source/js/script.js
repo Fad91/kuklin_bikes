07f68c431a17ba1ff83eb0af2784f234adc78149
Антон Куклин <homyak91@inbox.ru>
2021-07-12 02:08:25 +0400

    updt

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,16 @@
 // Файл скрипта
 
-//  Variables
+//  Variables - переменные
 
 const mainMenu = document.querySelector(".main-menu");
 const menuLinks = Array.from(document.querySelectorAll(".main-menu__item a"));
 const navButton = document.querySelector(".nav__button");
+const nameInput = document.querySelector("#name");
 const phoneInput = document.querySelector("#tel");
-const form = document.querySelector(".feedback-form");
 const header = document.querySelector(".header");
 const navLogo = document.querySelector(".nav__logo");
+const pageMain = document.querySelector(".main");
+const moveTo = new MoveTo();
 
 // Enable JS - добавление/удаление классов при включенном JS
 
@@ -18,31 +20,50 @@ mainMenu.classList.add("main-menu--closed");
 navButton.classList.remove("nav__button--nojs");
 navLogo.classList.remove("nav__logo--nojs");
 
-// Navigation
-// При нажатии на меню надо заблокировать скролл для body, и при нажатии на ссылку надо закрывать меню(?)
-// и разблокировать скролл(?)
+// Navigation - обработка событии с кнопкой навигации
 
 navButton.addEventListener("click", function () {
   mainMenu.classList.toggle("main-menu--closed");
   mainMenu.classList.toggle("main-menu--opened");
-  navButton.classList.toggle("nav__button--open")
+  navButton.classList.toggle("nav__button--open");
+  pageMain.classList.add("visually-hidden");
 });
 
-// Validation
+// Blocking-scroll - блокировка скролла при открытии меню
 
-form.addEventListener("invalid", function () {
-  return false;
+for (let menuLink of menuLinks) {
+  menuLink.addEventListener("click", function () {
+    if (pageMain.classList.contains("visually-hidden")) {
+      pageMain.classList.remove("visually-hidden");
+    }
+    mainMenu.classList.remove("main-menu--opened");
+    mainMenu.classList.add("main-menu--closed");
+    navButton.classList.remove("nav__button--open");
+  });
+}
+
+// Изменение placeholder при фокусе и отмене фокуса
+
+nameInput.addEventListener("focus", function () {
+  nameInput.placeholder = "";
 });
 
-phoneInput.addEventListener("invalid", function () {
-  let re = /^\d[\d\(\)\ -]{4,14}\d$/;
-  let valid = re.test(phoneInput.textContent);
-  if (valid) {
-    phoneInput.setCustomValidity("");
-  } else {
-    phoneInput.setCustomValidity("Поле должно состоять из цифр");
-  }
+nameInput.addEventListener("blur", function () {
+  nameInput.placeholder = "Введите Ваше Имя";
 });
 
-// Smooth scroll
+phoneInput.addEventListener("focus", function () {
+  phoneInput.placeholder = "Поле должно состоять из цифр";
+});
 
+phoneInput.addEventListener("blur", function () {
+  phoneInput.placeholder = "Введите Ваш номер телефона";
+});
+
+// Smooth scroll - плавный скролл
+
+const triggers = document.getElementsByClassName("js-trigger");
+
+for (let trigger of triggers) {
+  moveTo.registerTrigger(trigger);
+}
