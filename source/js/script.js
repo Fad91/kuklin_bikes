758b8a3b6b23b361c53eee49208385daf04e05af
Антон Куклин <homyak91@inbox.ru>
2021-07-11 16:45:10 +0400

    upd

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
 // Файл скрипта
+
 //  Variables
 
 const mainMenu = document.querySelector(".main-menu");
@@ -6,27 +7,26 @@ const menuLinks = Array.from(document.querySelectorAll(".main-menu__item a"));
 const navButton = document.querySelector(".nav__button");
 const phoneInput = document.querySelector("#tel");
 const form = document.querySelector(".feedback-form");
-let sections = Array.from(document.querySelectorAll("section"));
+const header = document.querySelector(".header");
+const navLogo = document.querySelector(".nav__logo");
+
+// Enable JS - добавление/удаление классов при включенном JS
+
+header.classList.remove("header--nojs");
+mainMenu.classList.remove("main-menu--nojs");
+mainMenu.classList.add("main-menu--closed");
+navButton.classList.remove("nav__button--nojs");
+navLogo.classList.remove("nav__logo--nojs");
 
 // Navigation
 // При нажатии на меню надо заблокировать скролл для body, и при нажатии на ссылку надо закрывать меню(?)
 // и разблокировать скролл(?)
 
-mainMenu.classList.add("main-menu--closed");
-
 navButton.addEventListener("click", function () {
   mainMenu.classList.toggle("main-menu--closed");
   mainMenu.classList.toggle("main-menu--opened");
   navButton.classList.toggle("nav__button--open")
-  // if (mainMenu.classList.contains("main-menu--opened")) {
-  //   console.log(1);
-  //   document.body.style.overflow = "hidden";
-  // } else {
-  //   document.body.style.overflow = "";
-  // }
 });
-
-// Blocking scroll
 
 // Validation
 
