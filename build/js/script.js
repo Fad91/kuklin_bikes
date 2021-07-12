3722ed18223df21dfee124dab43b8b3bd7b21d0a
Антон Куклин <homyak91@inbox.ru>
2021-07-12 20:11:10 +0400

    upd

---
+++ build/js/script.js
@@ -0,0 +1,71 @@
+// Файл скрипта
+
+//  Variables - переменные
+
+var mainMenu = document.querySelector(".main-menu");
+var menuLinks = Array.from(document.querySelectorAll(".main-menu__item a"));
+var navButton = document.querySelector(".nav__button");
+var nameInput = document.querySelector("#name");
+var phoneInput = document.querySelector("#tel");
+var header = document.querySelector(".header");
+var navLogo = document.querySelector(".nav__logo");
+var pageMain = document.querySelector(".main");
+var form = document.querySelector(".feedback-form");
+var moveTo = new MoveTo();
+
+// Enable JS - добавление/удаление классов при включенном JS
+
+header.classList.remove("header--nojs");
+mainMenu.classList.remove("main-menu--nojs");
+mainMenu.classList.add("main-menu--closed");
+navButton.classList.remove("nav__button--nojs");
+navLogo.classList.remove("nav__logo--nojs");
+
+// Navigation - обработка событии с кнопкой навигации
+
+navButton.addEventListener("click", function () {
+  mainMenu.classList.toggle("main-menu--closed");
+  mainMenu.classList.toggle("main-menu--opened");
+  navButton.classList.toggle("nav__button--open");
+  pageMain.classList.toggle("visually-hidden");
+  form.classList.toggle("visually-hidden");
+});
+
+// Blocking-scroll - блокировка скролла при открытии меню
+
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener("click", function () {
+    if (pageMain.classList.contains("visually-hidden")) {
+      pageMain.classList.remove("visually-hidden");
+    }
+    mainMenu.classList.remove("main-menu--opened");
+    mainMenu.classList.add("main-menu--closed");
+    navButton.classList.remove("nav__button--open");
+    form.classList.remove("visually-hidden");
+  });
+});
+// Изменение placeholder при фокусе и отмене фокуса
+
+nameInput.addEventListener("focus", function () {
+  nameInput.placeholder = "";
+});
+
+nameInput.addEventListener("blur", function () {
+  nameInput.placeholder = "Введите Ваше Имя";
+});
+
+phoneInput.addEventListener("focus", function () {
+  phoneInput.placeholder = "+7XXXXXXXXXX";
+});
+
+phoneInput.addEventListener("blur", function () {
+  phoneInput.placeholder = "Введите Ваш номер телефона";
+});
+
+// Smooth scroll - плавный скролл
+
+var triggers = document.getElementsByClassName("js-trigger");
+
+triggers.forEach(function (trigger) {
+  moveTo.registerTrigger(trigger);
+});
--- source/js/script.js
+++ source/js/script.js
@@ -1,72 +1,71 @@
 // Файл скрипта
-
+'use strict';
 //  Variables - переменные
 
-const mainMenu = document.querySelector(".main-menu");
-const menuLinks = Array.from(document.querySelectorAll(".main-menu__item a"));
-const navButton = document.querySelector(".nav__button");
-const nameInput = document.querySelector("#name");
-const phoneInput = document.querySelector("#tel");
-const header = document.querySelector(".header");
-const navLogo = document.querySelector(".nav__logo");
-const pageMain = document.querySelector(".main");
-const form = document.querySelector(".feedback-form");
-const moveTo = new MoveTo();
+var mainMenu = document.querySelector('.main-menu');
+var menuLinks = Array.from(document.querySelectorAll('.main-menu__item a'));
+var navButton = document.querySelector('.nav__button');
+var nameInput = document.querySelector('#name');
+var phoneInput = document.querySelector('#tel');
+var header = document.querySelector('.header');
+var navLogo = document.querySelector('.nav__logo');
+var pageMain = document.querySelector('.main');
+var form = document.querySelector('.feedback-form');
+var moveTo = new MoveTo();
 
 // Enable JS - добавление/удаление классов при включенном JS
 
-header.classList.remove("header--nojs");
-mainMenu.classList.remove("main-menu--nojs");
-mainMenu.classList.add("main-menu--closed");
-navButton.classList.remove("nav__button--nojs");
-navLogo.classList.remove("nav__logo--nojs");
+header.classList.remove('header--nojs');
+mainMenu.classList.remove('main-menu--nojs');
+mainMenu.classList.add('main-menu--closed');
+navButton.classList.remove('nav__button--nojs');
+navLogo.classList.remove('nav__logo--nojs');
 
 // Navigation - обработка событии с кнопкой навигации
 
-navButton.addEventListener("click", function () {
-  mainMenu.classList.toggle("main-menu--closed");
-  mainMenu.classList.toggle("main-menu--opened");
-  navButton.classList.toggle("nav__button--open");
-  pageMain.classList.toggle("visually-hidden");
-  form.classList.toggle("visually-hidden");
+navButton.addEventListener('click', function () {
+  mainMenu.classList.toggle('main-menu--closed');
+  mainMenu.classList.toggle('main-menu--opened');
+  navButton.classList.toggle('nav__button--open');
+  pageMain.classList.toggle('visually-hidden');
+  form.classList.toggle('visually-hidden');
 });
 
 // Blocking-scroll - блокировка скролла при открытии меню
 
-for (let menuLink of menuLinks) {
-  menuLink.addEventListener("click", function () {
-    if (pageMain.classList.contains("visually-hidden")) {
-      pageMain.classList.remove("visually-hidden");
+menuLinks.forEach(function (menuLink) {
+  menuLink.addEventListener('click', function () {
+    if (pageMain.classList.contains('visually-hidden')) {
+      pageMain.classList.remove('visually-hidden');
     }
-    mainMenu.classList.remove("main-menu--opened");
-    mainMenu.classList.add("main-menu--closed");
-    navButton.classList.remove("nav__button--open");
-    form.classList.remove("visually-hidden");
+    mainMenu.classList.remove('main-menu--opened');
+    mainMenu.classList.add('main-menu--closed');
+    navButton.classList.remove('nav__button--open');
+    form.classList.remove('visually-hidden');
   });
-}
-
+});
 // Изменение placeholder при фокусе и отмене фокуса
 
-nameInput.addEventListener("focus", function () {
-  nameInput.placeholder = "";
+nameInput.addEventListener('focus', function () {
+  nameInput.placeholder = '';
 });
 
-nameInput.addEventListener("blur", function () {
-  nameInput.placeholder = "Введите Ваше Имя";
+nameInput.addEventListener('blur', function () {
+  nameInput.placeholder = 'Введите Ваше Имя';
 });
 
-phoneInput.addEventListener("focus", function () {
-  phoneInput.placeholder = "+7XXXXXXXXXX";
+phoneInput.addEventListener('focus', function () {
+  phoneInput.placeholder = '+7XXXXXXXXXX';
 });
 
-phoneInput.addEventListener("blur", function () {
-  phoneInput.placeholder = "Введите Ваш номер телефона";
+phoneInput.addEventListener('blur', function () {
+  phoneInput.placeholder = 'Введите Ваш номер телефона';
 });
 
 // Smooth scroll - плавный скролл
 
-const triggers = document.getElementsByClassName("js-trigger");
+var triggers = document.getElementsByClassName('js-trigger');
 
-for (let trigger of triggers) {
+triggers.forEach(function (trigger) {
   moveTo.registerTrigger(trigger);
-}
+});
