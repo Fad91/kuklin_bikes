0f3637ee4829af8c251c8442fcf61aab600580f8
Антон Куклин <homyak91@inbox.ru>
2021-07-15 16:47:09 +0400

    updt

--- build/js/script.js
+++ build/js/script.js
@@ -12,8 +12,14 @@ var menuOpened = false;
 // Enable JS - добавление/удаление классов при включенном JS
 
 var addActiveStatement = function () {
-  header.classList.add('active');
-  mainMenu.classList.add('main-menu--closed');
+  if (header) {
+    header.classList.add('active');
+  }
+  if (mainMenu) {
+    mainMenu.classList.add('main-menu--closed');
+  } else {
+    return;
+  }
 };
 
 window.addEventListener('load', function () {
@@ -23,30 +29,40 @@ window.addEventListener('load', function () {
 // Navigation - обработка событии с кнопкой навигации и блокировка скролла
 
 var showMenu = function () {
-  mainMenu.classList.remove('main-menu--closed');
-  mainMenu.classList.add('main-menu--opened');
-  navButton.classList.add('nav__button--open');
-  // eslint-disable-next-line no-undef
-  bodyScrollLock.disableBodyScroll(mainMenu);
-  menuOpened = true;
+  if (mainMenu && navButton) {
+    mainMenu.classList.remove('main-menu--closed');
+    mainMenu.classList.add('main-menu--opened');
+    navButton.classList.add('nav__button--open');
+    // eslint-disable-next-line no-undef
+    bodyScrollLock.disableBodyScroll(mainMenu);
+    menuOpened = true;
+  } else {
+    return;
+  }
 };
 
 var hideMenu = function () {
-  mainMenu.classList.add('main-menu--closed');
-  mainMenu.classList.remove('main-menu--opened');
-  navButton.classList.remove('nav__button--open');
-  // eslint-disable-next-line no-undef
-  bodyScrollLock.enableBodyScroll(mainMenu);
-  menuOpened = false;
+  if (mainMenu && navButton) {
+    mainMenu.classList.add('main-menu--closed');
+    mainMenu.classList.remove('main-menu--opened');
+    navButton.classList.remove('nav__button--open');
+    // eslint-disable-next-line no-undef
+    bodyScrollLock.enableBodyScroll(mainMenu);
+    menuOpened = false;
+  } else {
+    return;
+  }
 };
 
-navButton.addEventListener('click', function () {
-  if (menuOpened) {
-    hideMenu();
-  } else {
-    showMenu();
-  }
-});
+if (navButton) {
+  navButton.addEventListener('click', function () {
+    if (menuOpened) {
+      hideMenu();
+    } else {
+      showMenu();
+    }
+  });
+}
 
 menuLinks.forEach(function (menuLink) {
   menuLink.addEventListener('click', function () {
@@ -56,13 +72,15 @@ menuLinks.forEach(function (menuLink) {
 
 // Изменение placeholder при фокусе и отмене фокуса
 
-phoneInput.addEventListener('focus', function () {
-  phoneInput.placeholder = '+7XXXXXXXXXX';
-});
+if (phoneInput) {
+  phoneInput.addEventListener('focus', function () {
+    phoneInput.placeholder = '+7XXXXXXXXXX';
+  });
 
-phoneInput.addEventListener('blur', function () {
-  phoneInput.placeholder = 'Введите Ваш номер телефона';
-});
+  phoneInput.addEventListener('blur', function () {
+    phoneInput.placeholder = 'Введите Ваш номер телефона';
+  });
+}
 
 // Smooth scroll - плавный скролл
 
